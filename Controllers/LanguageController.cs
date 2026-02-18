dfaae79e591dc07f28fbba3f1922e59641201c77
Yusuf Yavuz <ysffyavuzz@gmail.com>
2026-02-19 00:07:32 +0300

    değişiklikler yapıldı cursordan

---
+++ Controllers/LanguageController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmyGrupWeb.Controllers
+{
+    public class LanguageController : Controller
+    {
+        [HttpGet]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            // Güvenlik: boşsa home'a yönlendir
+            if (string.IsNullOrWhiteSpace(returnUrl))
+                returnUrl = "/";
+
+            // Cookie yaz
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions
+                {
+                    Expires = DateTimeOffset.UtcNow.AddYears(1),
+                    IsEssential = true,
+                    HttpOnly = false
+                }
+            );
+
+            return LocalRedirect(returnUrl);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Localization servisleri
+builder.Services.AddLocalization(options => {
+    options.ResourcesPath = "Resources";
+});
+
+builder.Services
+    .AddControllersWithViews()
+    .AddViewLocalization()
+    .AddDataAnnotationsLocalization();
+
+// Desteklenen diller
+var supportedCultures = new[] { "tr-TR", "en-US" };
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var cultures = supportedCultures.Select(c => new CultureInfo(c)).ToList();
+
+    options.DefaultRequestCulture = new RequestCulture("tr-TR");
+    options.SupportedCultures = cultures;
+    options.SupportedUICultures = cultures;
+
+    // Önce cookie oku, yoksa default tr-TR
+    options.RequestCultureProviders = new List<IRequestCultureProvider>
+    {
+        new CookieRequestCultureProvider()
+    };
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+// Localization middleware (routing'den önce)
+var locOptions = app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value;
+app.UseRequestLocalization(locOptions);
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
